948b5b5b929fa9bd9f8cd24a603489aa77ab1ac5
Enrique <enrique.gomez.electronico@gmail.com>
2023-10-06 00:57:27 +0200

    home page

--- docs-web/docusaurus.config.js
+++ docs-web/docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Pepperoni',
-  tagline: 'Pepperoni is cool',
+  tagline: 'Un robot de combate',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -39,17 +39,9 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
--- docs-web/src/pages/index.js
+++ docs-web/src/pages/index.js
@@ -14,13 +14,6 @@ function HomepageHeader() {
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
       </div>
     </header>
   );
@@ -34,7 +27,8 @@ export default function Home() {
       description="Pepperoni is a combat 'robot'.">
       <HomepageHeader />
       <main>
-        <HomepageFeatures />
+        <img src="/img/home_banner.jpg"/>
+        {/* <HomepageFeatures /> */}
       </main>
     </Layout>
   );
